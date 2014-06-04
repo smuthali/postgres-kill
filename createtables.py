0432b18f92d867478c97a7dd13ebfdfc6f21ef5d
smuthali <satish.muthali@gmail.com>
2014-06-04 11:11:23 -0700

    refactor createtables.py

--- createtables.py
+++ createtables.py
@@ -14,13 +14,13 @@ try:
   
     cur.execute("CREATE TABLE airlines(id INT PRIMARY KEY, name VARCHAR(100), name VARCHAR(100)")
     cur.execute("INSERT INTO airlines VALUES(1,'Air_Canada',Canada)")
-    cur.execute("INSERT INTO cars VALUES(2,'Lufthansa',Germany)")
-    cur.execute("INSERT INTO cars VALUES(3,'Aer_Lingus',Ireland)")
-    cur.execute("INSERT INTO cars VALUES(4,'Etihad',UAE)")
-    cur.execute("INSERT INTO cars VALUES(5,'Emirates',UAE)")
-    cur.execute("INSERT INTO cars VALUES(6,'Singapore_Airlines',Singapore)")
-    cur.execute("INSERT INTO cars VALUES(7,'Qantas',Australia)")
-    cur.execute("INSERT INTO cars VALUES(8,'Air_India',India)")
+    cur.execute("INSERT INTO airlines VALUES(2,'Lufthansa',Germany)")
+    cur.execute("INSERT INTO airlines VALUES(3,'Aer_Lingus',Ireland)")
+    cur.execute("INSERT INTO airlines VALUES(4,'Etihad',UAE)")
+    cur.execute("INSERT INTO airlines VALUES(5,'Emirates',UAE)")
+    cur.execute("INSERT INTO airlines VALUES(6,'Singapore_Airlines',Singapore)")
+    cur.execute("INSERT INTO airlines VALUES(7,'Qantas',Australia)")
+    cur.execute("INSERT INTO airlines VALUES(8,'Air_India',India)")
     
     con.commit()
     
